d7b99ce9e4fdba2522f0060d2abcd6b5f15b3ec1
Cris Jr <crisjr@pm.me>
2020-08-02 16:15:05 -0300

    Move player around

--- Assets/Controllers/PlayerController.cs
+++ Assets/Controllers/PlayerController.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody rigidBody;
+    public float playerSpeed = 50;
+
     void Start()
     {
 
@@ -9,6 +12,21 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-
+        if (Input.GetKey("a"))
+        {
+            rigidBody.AddForce(-playerSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            rigidBody.AddForce(playerSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            rigidBody.AddForce(0, 0, playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rigidBody.AddForce(0, 0, -playerSpeed * Time.deltaTime);
+        }
     }
 }
